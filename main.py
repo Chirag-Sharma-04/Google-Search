891bcf01e164a284ca0385992bec974cb66e4c88
Chirag Sharma <chiragcs2004@gmail.com>
2024-08-19 22:33:16 +0530

    Changes

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import webbrowser as wb
 #pip install google 
 
-try:
+while(True):
+
+ try:
     from googlesearch import search
-except ImportError:
+ except ImportError:
     print("No module named 'google' found")
  
-# to search
-query =(input("What would u like to search: "))
+ # to search
+ query =(input("What would u like to search: "))
  
-for j in search(query, tld="co.in", num=1, stop=1, pause=0):
+ for j in search(query, tld="co.in", num=1, stop=1, pause=0):
     print(j)
-url1=j
-wb.get().open_new(url1)
+ url1=j
+ wb.get().open_new(url1)
